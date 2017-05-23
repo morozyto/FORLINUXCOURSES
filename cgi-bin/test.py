4731f2f9db4218422030875a973de0395d1cf219
Anton Morozov <moranton@mail.ru>
2017-05-24 01:51:37 +0300

    kek

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+import time
 
+a = time.asctime()
 print ('''Content-type: text/html
 
+<html>
+<head>
+<title>DESCRIPTION</title>
+<meta http-equiv='refresh' content='10'>
+</head>
+<body>
 "Hello, World.''')
+print(a)
+print('''</body>
+</html>
+''')
