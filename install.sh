78027ee814cb74559a122b49db72212ff04b6c65
Anton Morozov <moranton@mail.ru>
2017-05-23 23:53:22 +0300

    hhhheh

--- install.sh
+++ install.sh
@@ -3,7 +3,17 @@
 	then sudo -s
 fi
 
-apt-get install apache2
-apt-get install nginx
+apt-get install python3 -y
 
+apt-get install apache2 -y
 
+cp apache2.conf /etc/apache2/apache2/conf
+cp 000-default.conf /etc/apache2/sites-available/000-default.conf
+mkdir /var/www/cgi-bin 
+cp test.py /var/www/cgi-bin/test.py
+a2enmod cgi
+/etc/init.d/apache2 restart
+
+apt-get install nginx -y
+cp nginx.conf /etc/nginx/nginx.conf
+nginx -s reload
